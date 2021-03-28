a69355714474b9cf67d8cd4824383f047937af12
Daniel Ch <>
2021-03-28 22:08:24 +0200

    add missing CurrentSensing.h

---
+++ ESPController/include/CurrentSensing.h
@@ -0,0 +1,12 @@
+// Currentsensing Options
+#define sensingNone     0
+#define sensingA0       1
+#define sensingINA228   2
+
+// what Currentsensing method used?
+#define CURRENTSENSING  sensingA0
+
+// How often the sensing is mesaured
+#define sensingIntervalInSeconds    2
+
+#define mAPerADC                (float)(39.0625F)
